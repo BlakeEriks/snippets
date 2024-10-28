92382646aa830c0b361793d09e9157e9f1e06049
Blake Eriks <blakeeriks.dev@gmail.com>
2024-10-27 21:05:26 -0400

    doink

--- app/routes/quotes.$quoteId.tsx
+++ app/routes/quotes.$quoteId.tsx
@@ -12,7 +12,6 @@ const QuoteRoute = () => {
   const { quote } = useSuperLoaderData<typeof loader>()
 
   if (!quote) return <div>Quote not found</div>
-
   return (
     <div className='max-w-3xl mx-auto p-6 rounded-lg shadow-md mt-8'>
       <div className='mb-8 text-center'>
